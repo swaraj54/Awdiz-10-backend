798f7f23abc83120e42898caacf87a01b337e201
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-03-29 12:50:35 +0530

    Updates for day 3

--- index.js
+++ index.js
@@ -2,17 +2,47 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//   if (req.body.email) {
+//     next();
+//   } else {
+//     return res.send("Email not found,");
+//   }
+// });
+
 app.get("/", (req, res) => {
-  res.send("Welcome to backend server.");
+  return res.send("Welcome to backend server.");
 });
 app.get("/books", (req, res) => {
-  res.send("My Books.");
+  return res.send("My Books.");
 });
 app.get("/products", (req, res) => {
-  res.send("My Products.");
+  return res.send("My Products.");
 });
 app.get("/hello", (req, res) => {
-  res.send("Hii.");
+  return res.send("Hii.");
+});
+
+app.post("/register", (req, res) => {
+  try {
+    // console.log(req.body.name, req.body.email);
+    const { name, email, password, confirmPassword } = req.body;
+    console.log(name, email, password, confirmPassword);
+
+    if (!name || !email || !password || !confirmPassword) {
+      return res.send("All data mandatory,");
+    }
+    if (password !== confirmPassword) {
+      return res.send("password not matched");
+    }
+
+    return res.send("Registrerationc omplted.");
+  } catch (error) {
+    console.log(error, "error in register api call.");
+    return res.send(error);
+  }
 });
 
 app.listen(8000, () => console.log("Server is running on port 8000"));
